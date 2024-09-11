90a3663b8ff80e5f27f1067805183b0aff1906f7
Ethan Peters <et8lg2pz@students.codeinstitute.net>
2024-09-11 21:15:25 +0000

    Add event listeners to buttons in script.js

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,3 +1,25 @@
+// Wait for the DOM to finish loading before running the game
+// Get the button elements and add event listeners to them
+
+document.addEventListener("DOMContentLoaded", function () {
+    let buttons = document.getElementsByTagName("button");
+    // get all the buttons in the HTML and store them in the variable buttons
+    for (let button of buttons) {
+        // loop through all the buttons and add an event listener to them
+        button.addEventListener("click", function () {
+            // add an event listener to the button that listens for a click
+            if (this.getAttribute("data-type") === "submit") {
+                // if the button clicked has a data-type of submit, alert "You clicked Submit!"
+                alert("You clicked Submit!"); // alert "You clicked Submit!" when the button is clicked
+            } else {
+                // if the button clicked does not have a data-type of submit, alert "You clicked [gameType]"
+                let gameType = this.getAttribute("data-type"); // get the data-type of the button clicked and store it in the variable gameType
+                alert(`You clicked ${gameType}`);
+            } // alert "You clicked [gameType]" when the button is clicked
+        });
+    }
+});
+
 function runGame() {}
 
 function checkAnswer() {}
